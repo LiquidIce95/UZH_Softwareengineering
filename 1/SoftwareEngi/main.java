264aa424aaedca8085b3075ab88b8130e1c76469
LiquidIce95 <davidsanchez062@gmail.com>
2024-03-07 13:44:12 +0100

    first commint

---
+++ 1/SoftwareEngi/main.java
@@ -0,0 +1,10 @@
+package SoftwareEngi;
+import SoftwareEngi.util;
+
+class mmain{
+    public static void main(String[] args){
+        util.print("hello there");
+
+    }
+}
+
---
+++ 1/SoftwareEngi/util.java
@@ -0,0 +1,7 @@
+package SoftwareEngi;
+
+public class util {
+    public static void print(String msg){
+        System.out.println(msg);
+    }
+}
